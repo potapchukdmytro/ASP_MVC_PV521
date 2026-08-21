3eb301faa3a0725f6db6c478fe306f25b4b50af8
potapchukdmytro <traiget@gmail.com>
2026-08-21 20:09:44 +0300

    Categories CRUD

--- 01_intro/Controllers/CategoryController.cs
+++ 01_intro/Controllers/CategoryController.cs
@@ -1,6 +1,8 @@
 ﻿using _01_intro.Data;
 using _01_intro.Models;
+using _01_intro.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace _01_intro.Controllers
 {
@@ -18,6 +20,91 @@ namespace _01_intro.Controllers
             IEnumerable<Category> categories = _context.Categories;
 
             return View(categories);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(CategoryCreateVM viewModel)
+        {
+            var category = new Category
+            {
+                Name = viewModel.Name!,
+                Image = viewModel.Image,
+                Description = viewModel.Description
+            };
+
+            await _context.Categories.AddAsync(category);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Update(int? id)
+        {
+            if(id == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            var category = await _context.Categories
+                .FirstOrDefaultAsync(c => c.Id == id);
+
+            if(category == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            var viewModel = new CategoryUpdateVM
+            {
+                Id = category.Id,
+                Name = category.Name,
+                Image = category.Image,
+                Description = category.Description
+            };
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Update(CategoryUpdateVM viewModel)
+        {
+            var category = new Category
+            {
+                Id = viewModel.Id,
+                Image = viewModel.Image,
+                Name = viewModel.Name!,
+                Description = viewModel.Description
+            };
+
+            _context.Categories.Update(category);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if(id != null)
+            {
+                var category = await _context.Categories
+                    .FirstOrDefaultAsync(c => c.Id == id);
+
+                if(category != null)
+                {
+                    _context.Categories.Remove(category);
+                    await _context.SaveChangesAsync();
+                }
+            }
+
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ 01_intro/ViewModels/CategoryCreateVM.cs
@@ -0,0 +1,9 @@
+﻿namespace _01_intro.ViewModels
+{
+    public class CategoryCreateVM
+    {
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? Image { get; set; }
+    }
+}
