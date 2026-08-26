21deff88a59b07dbb60d2a0000a3de9fab9162aa
potapchukdmytro <traiget@gmail.com>
2026-08-26 20:52:45 +0300

    products. Pagination

--- 05_validation_query/01_intro/Controllers/ProductController.cs
+++ 05_validation_query/01_intro/Controllers/ProductController.cs
@@ -1,12 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using _01_intro.Data;
+using _01_intro.Models;
+using _01_intro.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace _01_intro.Controllers
 {
     public class ProductController : Controller
     {
-        public IActionResult Catalog()
+        private readonly ApplicationDbContext _context;
+
+        public ProductController(ApplicationDbContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        public IActionResult Index(int page = 1)
+        {
+            IQueryable<Product> products = _context.Products;
+
+            int count = products.Count();
+            int pageSize = 18;
+            int pages = (int)Math.Ceiling((double)count / pageSize);
+
+            products = products.Skip((page - 1) * pageSize).Take(pageSize);
+
+            var vm = new CatalogVM
+            {
+                Products = products,
+                page = page,
+                pages = pages
+            };
+
+            return View(vm);
         }
     }
 }
